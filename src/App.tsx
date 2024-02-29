2ebecbd939af1f172093f9ced6e6f08996c64b42
Italo Sousa <italusousa@gmail.com>
2024-02-28 21:44:15 -0300

    chore: add preline

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Header } from './components/Header';
+import { Hero } from './components/Hero';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="h-screen flex flex-col">
+      <Header />
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', 'node_modules/preline/dist/*.js'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [require('@tailwindcss/forms'), require('preline/plugin')]
 };
